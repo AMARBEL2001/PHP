19eda8c28106e418d3de7ecf3e933300f358c72f
AMAR <Amarbelacel16@gmail.com>
2025-05-09 23:12:32 +0100

    data

---
+++ One.php
@@ -0,0 +1,28 @@
+<?php
+    echo "This message with double quotes"; 
+
+    echo 'This message with single quotes';
+
+    print "This message with print";
+
+    echo "This is a string with a newline character\n";
+
+    echo "This is a string with a tab character\t";
+
+    echo "This is a string with a backslash character\\";
+
+    echo "This is a string with a dollar sign character\$";
+
+    echo "This is a string with a double quote character\"";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
